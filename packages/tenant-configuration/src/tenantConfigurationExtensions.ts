d795eb241145e90dadc0fb65292dd8c146397158
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2022-09-14 14:57:15 +0300

    Removed tenant-configuration package

--- packages/tenant-configuration/src/tenantConfigurationExtensions.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) TotalSoft.
-// This source code is licensed under the MIT license.
-
-import * as tenantConfiguration from './tenantConfiguration'
-import { ConnectionInfo } from './types'
-
-export function getConnectionInfo(tenantId: string, connectionStringName: string): ConnectionInfo {
-  return tenantConfiguration.getValue(tenantId, `connectionStrings.${connectionStringName}`)
-}
