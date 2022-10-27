8611426cd126d223d0589ed96a3b600ec194f60f
drosca@totalsoft.ro <drosca@totalsoft.ro>
2022-10-27 17:04:35 +0300

    added new test and corrected after each

--- packages/message-bus/__tests__/transport/rusi.test.ts
+++ packages/message-bus/__tests__/transport/rusi.test.ts
@@ -1,5 +1,5 @@
 import { rusi } from '../../src'
-import { RusiConnection } from '../../src/transport/rusi/types'
+import { RusiConnection, RusiChannel } from '../../src/transport/rusi/types'
 
 jest.mock('@grpc/grpc-js')
 jest.mock('@grpc/proto-loader')
@@ -9,12 +9,16 @@ import grpcJs, { GrpcObject } from '@grpc/grpc-js'
 
 describe('Testing rusi transport connection', () => {
   let mockRusiClient: any = null
-  const channelMock = {
-    watchConnectivityState: jest.fn()
-  }
+  let channelMock: RusiChannel | null = null
 
   beforeEach(() => {
     protoLoader.loadSync = jest.fn(() => ({}))
+
+    channelMock = {
+      watchConnectivityState: jest.fn(),
+      getConnectivityState: jest.fn()
+    }
+
     mockRusiClient = {
       waitForReady: jest.fn((_deadline, callback) => {
         callback()
@@ -27,8 +31,10 @@ describe('Testing rusi transport connection', () => {
         cancel: jest.fn(),
         end: jest.fn(),
         write: jest.fn()
-      }))
+      })),
+      close: jest.fn()
     }
+
     grpcJs.loadPackageDefinition = jest.fn(
       () =>
         ({
@@ -45,7 +51,8 @@ describe('Testing rusi transport connection', () => {
     )
   })
 
-  afterEach(() => {
+  afterEach(async () => {
+    await rusi.disconnect()
     jest.resetAllMocks()
     jest.resetModules()
   })
@@ -68,14 +75,26 @@ describe('Testing rusi transport connection', () => {
     // arrange
 
     // act
+    const c3: RusiConnection[] = await Promise.all([rusi.connect(), rusi.connect(), rusi.connect()])
     const c1: RusiConnection = await rusi.connect()
     const c2: RusiConnection = await rusi.connect()
-    const c3: RusiConnection[] = await Promise.all([rusi.connect(), rusi.connect(), rusi.connect()])
 
     // assert
     expect(Object.is(c1._rusiClient, c2._rusiClient)).toBeTruthy()
     expect(Object.is(c1._rusiClient, c3[0]._rusiClient)).toBeTruthy()
     expect(Object.is(c1._rusiClient, c3[1]._rusiClient)).toBeTruthy()
     expect(Object.is(c1._rusiClient, c3[2]._rusiClient)).toBeTruthy()
+  })
+
+  test('protoLoader creates the definition used for GRPC package definition', async () => {
+    // arrange
+    const definition = { fake: {} }
+    protoLoader.loadSync = jest.fn(() => definition)
+
+    // act
+    await rusi.connect()
+
+    // assert
+    expect(grpcJs.loadPackageDefinition).toBeCalledWith(definition)
   })
 })
